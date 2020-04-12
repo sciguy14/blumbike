8b61f437884e92016952ca4b99527251c2c970bf
Jeremy Blum <jeremy@jeremyblum.com>
2020-04-12 16:35:16 -0700

    Now using Redis a value store for session data instead of global vars which obviously won't work with multiple threads managing the webserver

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import redis
 import dash
 from functools import wraps
 import datetime
@@ -9,19 +10,14 @@ import plotly
 from dash.dependencies import Input, Output
 from flask import request
 
-session_timestamps = []
-session_mph = []
-session_heartrate = []
-latest_data = {"t": 0,
-               "dyno_rpm": 0,
-               "bike_rpm": 0,
-               "bike_mph": 0,
-               "heart_bpm": 0}
-
 # Initialize the app
 app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
 server = app.server  # This is the Flask Parent Server that we can use to receive webhooks
+
+# Connect to Redis for persistent storage of session data
+r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+r.flushdb()
 
 app.layout = html.Div(
     children=[
@@ -38,7 +34,7 @@ app.layout = html.Div(
                      html.Div(className='eight columns div-for-charts bg-grey',
                               children=
                               [
-                                  dcc.Graph(id='live-update-graph'),
+                                  dcc.Graph(id='live-update-graph', config={'displayModeBar': False}),
                                   dcc.Interval(
                                       id='interval-component',
                                       interval=1000,  # in milliseconds
@@ -51,6 +47,7 @@ app.layout = html.Div(
     ]
 )
 
+
 # A decorator function to require an api key for pushing data to this application
 # https://coderwall.com/p/4qickw/require-an-api-key-for-a-route-in-flask-using-only-a-decorator
 def require_apikey(view_function):
@@ -58,11 +55,10 @@ def require_apikey(view_function):
     # the new, post-decoration function. Note *args and **kwargs here.
     def decorated_function(*args, **kwargs):
         if request.json['apikey'] and "apikey" in os.environ and request.json['apikey'] == str(os.environ.get("apikey")):
-            #print("matched api key")
             return view_function(*args, **kwargs)
         else:
             print("invalid api key match")
-            return {"success": False}, 401
+            return {"reply": "invalid key"}, 401
     return decorated_function
 
 
@@ -70,33 +66,40 @@ def require_apikey(view_function):
 @server.route('/append', methods=['POST'])
 @require_apikey
 def append_data():
-    global latest_data
-    global session_timestamps
-    global session_mph
-    global session_heartrate
     latest_data = json.loads(request.json['data'])
-    #print(latest_data)
-    session_timestamps.append(datetime.datetime.fromtimestamp(int(latest_data['t'])))
-    session_mph.append(latest_data['bike_mph'])
-    session_heartrate.append(latest_data['heart_bpm'])
-    #print(session_timestamps)
-    #print(session_mph)
-    #print("")
-    return {"success": True}
+
+    # If a value comes in out of order, discard it.
+    if r.exists('timestamp') and int(r.lindex('timestamp', 0)) > int(latest_data['t']):
+        print("IGNORED (STALE): {}".format(latest_data))
+        return {"reply": "ignored stale data"}
+
+    # Push the data into a running list in redis
+    r.lpush('timestamp', latest_data['t'])
+    r.lpush('bike_mph', latest_data['bike_mph'])
+    r.lpush('heart_bpm', latest_data['heart_bpm'])
+
+    # Keep the list trimmed
+    r.ltrim('timestamp', 0, 300)
+    r.ltrim('bike_mph', 0, 300)
+    r.ltrim('heart_bpm', 0, 300)
+
+    print("APPENDED: {}".format(latest_data))
+    return {"reply": "data appended"}
 
 
 @app.callback(Output('live-update-text', 'children'),
               [Input('interval-component', 'n_intervals')])
 def update_metrics(n):
-    global latest_data
-    timestamp = datetime.datetime.fromtimestamp(int(latest_data['t'])).strftime('%c')
-    style = {'padding': '5px', 'fontSize': '16px'}
+    if r.exists('timestamp'):
+        style = {'padding': '5px', 'fontSize': '16px'}
+        return [
+            html.P('Last Update: {}'.format(datetime.datetime.fromtimestamp(int(r.lindex('timestamp', 0))).strftime('%c')), style=style),
+            html.P('Bike Speed: {0:0.2f} MPH'.format(float(r.lindex('bike_mph', 0))), style=style),
+            html.P('Heart Rate: {0:0.2f} BPM'.format(float(r.lindex('heart_bpm', 0))), style=style)
+        ]
+    style = {'padding': '5px', 'fontSize': '16px', 'fontStyle': 'italic'}
     return [
-        html.H2('Last Update: {}'.format(timestamp)),
-        html.P('Dyno RPM: {0:0.2f} RPM'.format(latest_data['dyno_rpm']), style=style),
-        html.P('Bike RPM: {0:0.2f} RPM'.format(latest_data['bike_rpm']), style=style),
-        html.P('Bike Speed: {0:0.2f} MPM'.format(latest_data['bike_mph']), style=style),
-        html.P('Heartrate: {0:0.2f} BPM'.format(latest_data['heart_bpm']), style=style)
+        html.P('Waiting to receive data from bike...', style=style)
     ]
 
 
@@ -104,16 +107,11 @@ def update_metrics(n):
 @app.callback(Output('live-update-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_graph_live(n):
-    data = {
-        'timestamp': session_timestamps,
-        'speed': session_mph,
-        'heartrate': session_heartrate,
-    }
-
     # Create the graph with subplots
     fig = plotly.subplots.make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.3, subplot_titles=("Bike Speed", "Heart Rate"))
     fig.update_layout(
         xaxis=dict(
+            fixedrange=True,
             title_font=dict(
                 size=14,
                 color='grey',
@@ -131,6 +129,7 @@ def update_graph_live(n):
             ),
         ),
         xaxis2=dict(
+            fixedrange=True,
             title_font=dict(
                 size=14,
                 color='grey',
@@ -148,6 +147,7 @@ def update_graph_live(n):
             ),
         ),
         yaxis=dict(
+            fixedrange=True,
             title_font=dict(
                 size=14,
                 color='grey',
@@ -166,6 +166,7 @@ def update_graph_live(n):
             ),
         ),
         yaxis2=dict(
+            fixedrange=True,
             title_font=dict(
                 size=14,
                 color='grey',
@@ -197,29 +198,35 @@ def update_graph_live(n):
         paper_bgcolor='rgba(0,0,0,0)'
     )
 
-    fig.append_trace({
-        'x': data['timestamp'],
-        'y': data['speed'],
-        'text': data['speed'],
-        'name': 'Bike Speed',
-        'mode': 'lines+markers',
-        'type': 'scatter',
-        'line': dict(color='royalblue', width=4),
-        'marker': dict(color='royalblue', size=12),
-    }, 1, 1)
-    fig.append_trace({
-        'x': data['timestamp'],
-        'y': data['heartrate'],
-        'text': data['heartrate'],
-        'name': 'Heart Rate',
-        'mode': 'lines+markers',
-        'type': 'scatter',
-        'line': dict(color='firebrick', width=4),
-        'marker': dict(color='firebrick', size=12),
-    }, 2, 1)
-
     for i in fig['layout']['annotations']:
         i['font'] = dict(size=18, color='grey')
+
+    if r.exists('timestamp'):
+        data = {
+            'timestamp': [datetime.datetime.fromtimestamp(int(x)) for x in r.lrange('timestamp', 0, -1)],
+            'speed': [float(i) for i in r.lrange('bike_mph', 0, -1)],
+            'heartrate': [float(i) for i in r.lrange('heart_bpm', 0, -1)]
+        }
+        fig.append_trace({
+            'x': data['timestamp'],
+            'y': data['speed'],
+            'text': data['speed'],
+            'name': 'Bike Speed',
+            'mode': 'lines+markers',
+            'type': 'scatter',
+            'line': dict(color='royalblue', width=2),
+            'marker': dict(color='royalblue', size=6),
+        }, 1, 1)
+        fig.append_trace({
+            'x': data['timestamp'],
+            'y': data['heartrate'],
+            'text': data['heartrate'],
+            'name': 'Heart Rate',
+            'mode': 'lines+markers',
+            'type': 'scatter',
+            'line': dict(color='firebrick', width=2),
+            'marker': dict(color='firebrick', size=6),
+        }, 2, 1)
 
     return fig
 
